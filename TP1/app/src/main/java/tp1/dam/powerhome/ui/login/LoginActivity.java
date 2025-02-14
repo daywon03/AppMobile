ac6bfda98bcd3a1df70265f377147d821fdde98a
Damon Ba <damonba@MacBook-Air-de-Damon-2.local>
2025-02-14 09:00:59 +0100

    tp1

---
+++ TP1/app/src/main/java/tp1/dam/powerhome/ui/login/LoginActivity.java
@@ -0,0 +1,41 @@
+package tp1.dam.powerhome.ui.login;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import tp1.dam.powerhome.R;
+import tp1.dam.powerhome.ui.login.LoginViewModel;
+import tp1.dam.powerhome.ui.login.LoginViewModelFactory;
+import tp1.dam.powerhome.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+    private ActivityLoginBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+}
